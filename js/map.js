b5ef32b8930e8892e1de8a03a3095105cdd4a755
ChJL <linjaak@gmail.com>
2023-01-06 23:42:20 +0100

    seperate city and town

--- js/map.js
+++ js/map.js
@@ -1,10 +1,15 @@
 // var map = L.map('mapid').setView([51.505, -0.09], 13);
 
-var markerIcon = L.icon({
-	iconUrl: 'images/marker-icon-2x.png',
+var cityIcon = L.icon({
+	iconUrl: 'images/marker/solid-blue.png',
 	shadowUrl: 'leaf-shadow.png',
 
-	iconSize:     [15, 24], // size of the icon
+	iconSize:     [17, 17], // size of the icon
+});
+
+var townIcon = L.icon({
+	iconUrl: 'images/marker/location-pin.png',
+	iconSize:     [17, 17], // size of the icon
 });
 
 var mapOptions = {
@@ -26,19 +31,33 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 
 const cities = [
-    {name: "Paris", lat: 48.8566, lng: 2.3522},
-    // {name: "New York", lat: 40.7128, lng: -74.0060},
+    {name: "Amsterdam", lat: 52.370216, lng: 4.895168},
+    {name: "Utrecht", lat: 52.090736, lng: 5.121420},
+    {name: "Rotterdam", lat: 51.924419, lng: 4.477733},
+
+    {name: "Frankfurt", lat: 50.110924, lng: 8.682127},
+    {name: "Cologne", lat: 50.937531, lng: 6.960279},
     {name: "London", lat: 51.5074, lng: -0.1278},
     // {name: "Tokyo", lat: 35.6895, lng: 139.6917},
+    // {name: "Paris", lat: 48.8566, lng: 2.3522},
+    // {name: "New York", lat: 40.7128, lng: -74.0060},
     // Add more cities here...
+  ];
+  const towns = [
+    {name: "Giethoorn", lat: 52.739700, lng: 6.077420}
   ];
 
   cities.forEach((city) => {
     const popupContent = document.createElement("div")
-    popupContent.innerHTML = "<img src='" + "images/"+ city.name + ".jpg "+ "'>"
-    const marker = L.marker([city.lat, city.lng],{icon: markerIcon}).bindPopup(popupContent,
+    popupContent.innerHTML = "<h3>" + city.name +"</h3>" + "<img src='" + "images/cities/"+ city.name + ".jpg "+ "'>"
+    const marker = L.marker([city.lat, city.lng],{icon: cityIcon}).bindPopup(popupContent,
         { maxWidth: "auto" }).addTo(mymap);
   });
 
-
+towns.forEach((town) => {
+  const popupContent = document.createElement("div")
+  popupContent.innerHTML = "<h3>" + town.name +"</h3>" + "<img src='" + "images/towns/"+ town.name + ".jpg "+ "'>"
+  const marker = L.marker([town.lat, town.lng],{icon: townIcon}).bindPopup(popupContent,
+      { maxWidth: "auto" }).addTo(mymap);
+});
 
